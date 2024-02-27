efe4a1819084d4aa14f8c85423c94b4e330f64f3
Patrik Hellberg <patrik@hellberg.io>
2024-02-27 19:41:20 +0000

    add full screen capabilities

--- components/ToolbarIcons.tsx
+++ components/ToolbarIcons.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-import SVG, { Export, GearSix, Info } from '@hellberg/react-svg-icons'
-import { useContext } from 'react'
+import SVG, {
+  ArrowsIn,
+  ArrowsOut,
+  Export,
+  GearSix,
+  Info,
+} from '@hellberg/react-svg-icons'
+import { useContext, useState } from 'react'
 import { AppContext } from './AppContext'
 
 const ToolbarIcons = () => {
@@ -10,28 +16,59 @@ const ToolbarIcons = () => {
     computed: { foreground },
   } = useContext(AppContext)
 
+  const [isFullScreen, setIsFullScreen] = useState(false)
+
   const icons = [
     {
       action: () => dispatch({ type: 'SET_SHARE_MODAL', data: true }),
       id: 'open_shareModalOpen',
       icon: Export,
+      title: 'Share',
     },
     {
       action: () => dispatch({ type: 'SET_INFO_MODAL', data: true }),
       id: 'open_infoModalOpen',
       icon: Info,
+      title: 'Information',
     },
     {
       action: () => dispatch({ type: 'SET_TOOLBAR', data: true }),
       id: 'open_toolbar',
       icon: GearSix,
+      title: 'Settings',
     },
   ]
 
+  const makeFullScreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen()
+      setIsFullScreen(true)
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen()
+      setIsFullScreen(false)
+    }
+  }
+
   return (
     <div className='absolute top-4 right-4 flex gap-4'>
+      <button
+        onClick={makeFullScreen}
+        title={isFullScreen ? 'Exit full screen' : 'Full screen'}
+      >
+        <SVG
+          stroke={foreground}
+          icon={isFullScreen ? ArrowsIn : ArrowsOut}
+          className='cursor-pointer'
+          pathClassName='pointer-events-none'
+        />
+      </button>
       {icons.map((icon) => (
-        <button onClick={icon.action} id={icon.id} key={icon.id}>
+        <button
+          onClick={icon.action}
+          id={icon.id}
+          key={icon.id}
+          title={icon.title}
+        >
           <SVG
             stroke={foreground}
             icon={icon.icon}
